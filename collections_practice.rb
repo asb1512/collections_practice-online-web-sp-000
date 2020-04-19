9c12253a2b5b18d72fc593641ef670804f6f544d
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-19 05:42:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -36,6 +36,7 @@ def kesha_maker(array)
     split_element_to_array = element.split("")
     split_element_to_array[2] = "$"
     joined_array = split_element_to_array.join
+    joined_array
     binding.pry
   end
 end
